303419427d645e872fd7082c1b094d6eb1d487fc
Chunming Zhou <david1.zhou@amd.com>
2019-02-19 17:29:31 +0800

    fence-chian: fix iterate chain node
    Signed-off-by: Chunming Zhou <david1.zhou@amd.com>

--- include/linux/dma-fence-chain.h
+++ include/linux/dma-fence-chain.h
@@ -69,7 +69,7 @@ to_dma_fence_chain(struct dma_fence *fence)
  */
 #define dma_fence_chain_for_each(iter, head)	\
 	for (iter = dma_fence_get(head); iter; \
-	     iter = dma_fence_chain_walk(head))
+	     iter = dma_fence_chain_walk(iter))
 
 struct dma_fence *dma_fence_chain_walk(struct dma_fence *fence);
 int dma_fence_chain_find_seqno(struct dma_fence **pfence, uint64_t seqno);
